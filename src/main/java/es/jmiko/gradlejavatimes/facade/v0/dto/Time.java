359a3b59247820d018c232481bfa55441ed66174
Jose Miko Bonaba <mrtripode@gmail.com>
2020-03-08 21:07:38 +0100

    [UPDATED] Se añade el dto Time en la versión 0 del API en la fachada

---
+++ src/main/java/es/jmiko/gradlejavatimes/facade/v0/dto/Time.java
@@ -0,0 +1,98 @@
+package es.jmiko.gradlejavatimes.facade.v0.dto;
+
+import java.io.Serializable;
+
+public class Time implements Serializable {
+
+    private String hour;
+    private String minute;
+    private String second;
+    private String date;
+    private String day;
+    private String dayOfTheWeek;
+    private String month;
+    private String monthName;
+    private String year;
+    private String time;
+
+    public String getHour() {
+        return hour;
+    }
+
+    public void setHour(String hour) {
+        this.hour = hour;
+    }
+
+    public String getMinute() {
+        return minute;
+    }
+
+    public void setMinute(String minute) {
+        this.minute = minute;
+    }
+
+    public String getSecond() {
+        return second;
+    }
+
+    public void setSecond(String second) {
+        this.second = second;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getDayOfTheWeek() {
+        return dayOfTheWeek;
+    }
+
+    public void setDayOfTheWeek(String dayOfTheWeek) {
+        this.dayOfTheWeek = dayOfTheWeek;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getMonthName() {
+        return monthName;
+    }
+
+    public void setMonthName(String monthName) {
+        this.monthName = monthName;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+}
